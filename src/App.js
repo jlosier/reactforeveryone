f3abefea94aa9267ec60ea64511b835f9c8007cb
Joe Osier <osierjos@gmail.com>
2021-05-13 21:44:27 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import PropTypes from "prop-types"
 import './App.css';
+import { Counter } from './Counter'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">    
+        <Counter />
     </div>
   );
 }
 
+function HelloWorld({name, greeting = "Hello"}) {
+  return (
+    <div>
+      <h1>{greeting}, {name}!</h1>
+    </div>
+  );
+}
+
+HelloWorld.propTypes = {
+  name: PropTypes.string,
+  greeting: PropTypes.string
+}
+
 export default App;
+
