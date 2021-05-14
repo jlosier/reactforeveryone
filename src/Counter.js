6f00792591a5e09a02092a1e3bbb86c7c3a3655f
Joe Osier <osierjos@gmail.com>
2021-05-13 21:44:56 -0400

    first commit

---
+++ src/Counter.js
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+export function Counter() {
+
+    // const [ state, setState] = useState(defaultState)
+    const [ count, setCount ] = useState(0);
+
+
+    return (
+        <div>
+            <h3>{count}</h3>
+            <button onClick={() => setCount(count +  1)} >+</button>
+            <button onClick={() => setCount(count -  1)} >-</button>
+            <button onClick={() => setCount(0)}>RESET</button>
+        </div>
+    );
+}
