b251354b2080e71a6c85543b0c4149e76f0dff3f
Joe Osier <osierjos@gmail.com>
2021-05-14 21:24:46 -0400

    lesson #7

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import PropTypes from "prop-types"
 import './App.css';
 import { Counter } from './Counter'
 import { Accordion } from './Accordion'
+import { Input } from './Input'
 
 function App() {
   return (
     <div className="App">    
+        <Input />
         <Counter />
         <Accordion />
     </div>
