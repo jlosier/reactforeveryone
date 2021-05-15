db20dd07da005927a85f94379f26e33ead1bc191
Joe Osier <osierjos@gmail.com>
2021-05-14 21:10:21 -0400

    lesson #6

---
+++ src/Accordion.js
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+export function Accordion() {
+    const [ isToggled, setIsToggled ] = useState(false)
+
+    return (
+        <div>
+            { isToggled ? <h3>Show Me</h3> : null }
+            <button onClick={ () => setIsToggled(!isToggled) }>Toggle</button>
+        </div>
+    )
+}
--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import PropTypes from "prop-types"
 import './App.css';
 import { Counter } from './Counter'
+import { Accordion } from './Accordion'
 
 function App() {
   return (
     <div className="App">    
         <Counter />
+        <Accordion />
     </div>
   );
 }
