b74aed1b23fc6948d03fb27aa72dc92ac7f1b1ad
Joe Osier <osierjos@gmail.com>
2021-05-18 20:46:47 -0400

    lesson #11 - filtering with state

--- src/MoviesList.js
+++ src/MoviesList.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 const movies = [
     {
         name: "Batman"
@@ -11,14 +13,22 @@ const movies = [
 ]
 
 export function MoviesList() {
+    const [ filter, setFilter ] = useState("")
+
     return (
         <div>
+            <label>
+                Filter: 
+                <input onChange={(e) => setFilter(e.target.value)} value={filter} />
+            </label>
             <ul>
-                { movies.map((movie) => {
-                    return (
-                        <li key={movie.name}>{movie.name}</li>
-                    )
-                })}
+                { movies
+                    .filter((movie) => movie.name.toLowerCase().includes(filter.toLowerCase()))
+                    .map((movie) => {
+                        return (
+                            <li key={movie.name}>{movie.name}</li>
+                        )
+                    })}
             </ul>
         </div>
     )
